887c80af119e8a1b5c19405aecb68393e1fecade
Parthtarpara <parthtarpara31@gmail.com>
2026-03-14 12:51:22 +0530

    Remove all files from repository

--- src/store/useAppStore.js
+++
@@ -1,9 +0,0 @@
-import { create } from 'zustand';
-import { users } from '../data/users';
-
-export const useAppStore = create((set) => ({
-    currentUser: users[0], // pre-login with admin
-    isAuthenticated: true,
-    login: (user) => set({ currentUser: user, isAuthenticated: true }),
-    logout: () => set({ currentUser: null, isAuthenticated: false }),
-}));
